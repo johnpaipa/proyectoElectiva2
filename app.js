7471772a11250e58b2f9a50c9c5b76d775fe3b89
Daniel Lagos <d.f.l.m.0126@gmail.com>
2021-12-02 21:52:53 -0500

    create routes files

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dbConnection = require('./database/config');
 
 const app = express();
 
+dbConnection()
+  .then()
+  .catch();
 
 app.use(cors());
 
@@ -12,6 +16,9 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api/product', require('./routes/product'));
+app.use('/api/detail', require('./routes/deatil'));
+app.use('/api/bill', require('./routes/bill'));
 
 app.listen(process.env.PORT, () => {
   console.log(`The sever is running in port ${process.env.PORT}`);
