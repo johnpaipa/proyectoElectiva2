ee4d6b42c9e1542a300f2d6facc03cc593259ef4
Daniel Lagos <d.f.l.m.0126@gmail.com>
2021-12-02 23:27:47 -0500

    create bill CRUD

--- controllers/bill.js
+++ controllers/bill.js
@@ -1,6 +1,140 @@
-const getBill = async () => {
+const { response } = require('express');
 
+const Bill = require('../models/Bill');
+
+const getBills = async (req, res = response) => {
+  try {
+    const bills = await Bill.find();
+    return res.status(200).json({
+      success: true,
+      bills
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      success: false,
+      message: 'Contact with admin'
+    });
+  }
+};
+
+const getBill = async (req, res = response) => {
+  try {
+    const { id } = req.params;
+
+    const bill = await Bill.find({ _id: id });
+
+    if (!bill) {
+      return res.status(400).json({
+        success: false,
+        message: 'This Bill doesn\'t exist'
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      bill
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      success: false,
+      message: 'Contact with admin'
+    });
+  }
+};
+
+const createBill = async (req, res = response) => {
+  try {
+    //TODO: look key duplicates
+
+    /*
+    if (bill) {
+      return res.status(400).json({
+        success: false,
+        message: 'Bill duplicate or anything else'
+      });
+    }
+    */
+
+    const bill = new Bill(req.body);
+
+    await bill.save();
+
+    return res.status(200).json({
+      success: true,
+      bill
+    });
+
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      success: false,
+      message: 'Contact with admin'
+    });
+  }
+};
+
+const updateBill = async (req, res = response) => {
+  try {
+    const { id } = req.params;
+    const { bId, ...rest } = req.body;
+
+    const bill = await Bill.findOneAndUpdate({ _id: id }, rest,
+      { new: true, useFindAndModify: false });
+
+    if (!bill) {
+      return res.status(400).json({
+        success: false,
+        message: 'This Bill doesn\'t exist'
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      bill
+    });
+
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      success: false,
+      message: 'Contact with admin'
+    });
+  }
+};
+
+const deleteBill = async (req, res = response) => {
+  try {
+    const { id } = req.params;
+
+    const bill = await Bill.findByIdAndDelete(id);
+
+    if (!bill) {
+      return res.status(400).json({
+        success: false,
+        message: 'This Bill doesn\'t exist'
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      bill
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      success: false,
+      message: 'Contact with admin'
+    });
+  }
 };
 
 
-module.exports = { getBill };
+module.exports = {
+  getBill,
+  getBills,
+  deleteBill,
+  createBill,
+  updateBill
+};
