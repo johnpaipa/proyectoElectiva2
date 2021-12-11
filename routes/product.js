cce689ff42db334cc9ba1314279df08eeb683b3e
Camilo Cortes <67564515+CamiloCortes1753@users.noreply.github.com>
2021-12-11 17:24:54 -0500

    Add files via upload

--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,17 @@
 const { Router } = require('express');
-const { getProduct } = require('../controllers/product');
+const controller = require('../controllers/product');
 
 const router = Router();
 
-router.get('/', [], getProduct);
+
+router.get('/', [], controller.getProducts);
+
+router.get('/:id', [], controller.getProduct);
+
+router.post('/', [], controller.createProduct);
+
+router.put('/:id', [], controller.updateProduct);
+
+router.delete('/:id', [], controller.deleteProduct);
 
 module.exports = router;
