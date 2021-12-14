a73ef83ca3530b9cca14899fc486c65d1d45760e
Daniel Lagos <d.f.l.m.0126@gmail.com>
2021-12-14 09:51:03 -0500

    merge changes with product routes

--- controllers/product.js
+++ controllers/product.js
@@ -2,35 +2,59 @@ const { request, response } = require('express');
 const { isValidDate } = require('../middleware/validate-fields');
 const Product = require('../models/Product');
 
+const getProduct = async (req, res = response) => {
+  try {
+    const { id } = req.params;
 
-const getProduct = async (req = request, res = response) => {
+    const product = await Product.findOne({ _id: id });
 
-    const [elements, products] = await Promise.all([
-        Product.countDocuments(),
-        Product.find()
-    ])
-
-    if (!products) {
-        return res.status(400).json({
-            success: false,
-            message: 'No found products'
-        })
+    if (!product) {
+      return res.status(400).json({
+        success: false,
+        message: 'This Product doesn\'t exist'
+      });
     }
-    res.status(400).json({
-        success: true,
-        elements,
-        products
-    })
+
+    return res.status(200).json({
+      success: true,
+      product
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      success: false,
+      message: 'Contact with admin'
+    });
+  }
+};
+
+const getProducts = async (req = request, res = response) => {
+  const [elements, products] = await Promise.all([
+    Product.countDocuments(),
+    Product.find()
+  ]);
+
+  if (!products) {
+    return res.status(400).json({
+      success: false,
+      message: 'No found products'
+    });
+  }
+  res.status(400).json({
+    success: true,
+    elements,
+    products
+  });
 };
 
 const createProduct = async (req = request, res = response) => {
-    const newProduct = new Product(req.body);
-    newProduct.save();
-    res.status(200).json({
-        success: true,
-        message: 'Product created',
-        product: newProduct
-    })
+  const newProduct = new Product(req.body);
+  newProduct.save();
+  res.status(200).json({
+    success: true,
+    message: 'Product created',
+    product: newProduct
+  });
 };
 
 // const isExpided = () => {
@@ -40,9 +64,65 @@ const createProduct = async (req = request, res = response) => {
 // const calcIva = () => {
 
 // }
+const updateProduct = async (req, res = response) => {
+  try {
+    const { id } = req.params;
+    const { bId, ...rest } = req.body;
 
-module.exports = {
-    getProduct,
-    createProduct
+    const product = await Product.findOneAndUpdate({ _id: id }, rest,
+      { new: true, useFindAndModify: false });
+
+    if (!product) {
+      return res.status(400).json({
+        success: false,
+        message: 'This Product doesn\'t exist'
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      product
+    });
+
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      success: false,
+      message: 'Contact with admin'
+    });
+  }
 };
 
+const deleteProduct = async (req, res = response) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      return res.status(400).json({
+        success: false,
+        message: 'This Bill doesn\'t exist'
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      product
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      success: false,
+      message: 'Contact with admin'
+    });
+  }
+};
+
+module.exports = {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct
+};
--- routes/product.js
+++ routes/product.js
@@ -1,24 +1,43 @@
 const { Router } = require('express');
-const { getProduct, createProduct } = require('../controllers/product');
-const { validateFields, validateProduct, validateNumber, validateDate } = require('../middleware/validate-fields');
+const {
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProducts
+} = require(
+  '../controllers/product');
+const {
+  validateFields,
+  validateProduct,
+  validateNumber,
+  validateDate
+} = require('../middleware/validate-fields');
 const { check } = require('express-validator');
-
 
 const router = Router();
 
-router.get('/', getProduct);
+router.get('/', [], getProducts);
 
 router.post('/new', [
-    check('idProduct', 'idProduct is required').not().isEmpty(),
-    check('description', 'description is required').not().isEmpty(),
-    check('value', 'value is required').isNumeric(),
-    check('stock', 'stock is required').isNumeric(),
-    check('typeProduct', 'typeProduct is required').not().isEmpty(),
-    check('dateExpired', 'dateExpired is required').not().isEmpty(),
-    check('dateExpired').custom(date => validateDate(date)),
-    check('stock').custom(stock => validateNumber(stock)),
-    check('typeProduct').custom(typeProduct => validateProduct(typeProduct)),
-    validateFields
+  check('idProduct', 'idProduct is required').not().isEmpty(),
+  check('description', 'description is required').not().isEmpty(),
+  check('value', 'value is required').isNumeric(),
+  check('stock', 'stock is required').isNumeric(),
+  check('typeProduct', 'typeProduct is required').not().isEmpty(),
+  check('dateExpired', 'dateExpired is required').not().isEmpty(),
+  check('dateExpired').custom(date => validateDate(date)),
+  check('stock').custom(stock => validateNumber(stock)),
+  check('typeProduct').custom(typeProduct => validateProduct(typeProduct)),
+  validateFields
 ], createProduct);
+
+router.get('/:id', [], getProduct);
+
+router.post('/', [], createProduct);
+
+router.put('/:id', [], updateProduct);
+
+router.delete('/:id', [], deleteProduct);
 
 module.exports = router;
