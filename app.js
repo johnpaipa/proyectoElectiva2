fd47d1e9cb1f57e9c56d76cafa989e0f4fdf5a94
Camilo Cortes <67564515+CamiloCortes1753@users.noreply.github.com>
2021-12-14 20:53:54 -0500

    new module documentation

--- app.js
+++ app.js
@@ -2,6 +2,29 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const dbConnection = require('./database/config');
+const path = require("path");
+
+
+//swagger
+const swaggerUI = require("swagger-ui-express");
+const swaggerjsDoc = require("swagger-jsdoc");
+
+const swaggerSpec={
+  definition:{
+    openapi:"3.0.0",
+    info:{
+      title: "Node mongoDB API",
+      version: "1.0.0"
+    },
+    servers:[
+      {
+        url: "http:localhost:8080"
+      }
+    ]
+  },
+  apis:[`${path.join(__dirname, "./routes/*.js")}`],
+}
+
 
 const app = express();
 
@@ -13,12 +36,17 @@ app.use(cors());
 
 app.use(express.static('public'));
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/product', require('./routes/product'));
 app.use('/api/detail', require('./routes/detail'));
 app.use('/api/bill', require('./routes/bill'));
+app.use(
+  '/api-doc',
+   swaggerUI.serve, 
+   swaggerUI.setup(swaggerjsDoc(swaggerSpec)))
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`The sever is running in port ${process.env.PORT}`);
